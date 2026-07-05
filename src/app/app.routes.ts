228d04850b55d7c7db52e16f60b537e442e5a5db
aaron lopez <aaron_lopez222@hotmail.com>
2026-07-04 23:34:25 -0600

    feat: Implement country app routing and layout
    - Added HomePages component as the main entry point.
    - Created country routes with lazy loading for country-related pages.
    - Introduced CountryLayout component to manage country page layout.
    - Added ByCapitalPage component for displaying country information by capital.
    - Implemented Footer component for consistent footer across the app.
    - Updated app component to include the footer.
    - Removed outdated app.spec.ts test file.
    - Created home page with a hero section and navigation button to country section.
    - Updated global styles to include DaisyUI plugin.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomePages } from './shared/pages/home-pages/home-pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: HomePages
+    },
+    {
+        path: 'country',
+        loadChildren: () => import('./country/country.routes')
+    },
+    {
+        path: '**',
+        redirectTo: ''
+
+    }
+];
---
+++ src/app/shared/pages/home-pages/home-pages.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home-pages',
+  imports: [RouterLink],
+  templateUrl: './home-pages.html',
+})
+export class HomePages {}
