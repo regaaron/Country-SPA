805ca89aaa52884ced55b9dad9ef5775993c471f
aaron lopez <aaron_lopez222@hotmail.com>
2026-07-08 02:34:34 -0600

    feat: Add country service and implement search by capital functionality

--- src/app/country/pages/by-capital-page/by-capital-page.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SearchInput } from "../../../shared/components/search-input/search-input";
 import { CountryLayout } from "../../layout/country-layout/country-layout";
 import { CountryList } from '../../components/country-list/country-list';
+import { Country } from '../../services/country';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -9,8 +10,14 @@ import { CountryList } from '../../components/country-list/country-list';
   templateUrl: './by-capital-page.html',
 })
 export class ByCapitalPage {
+
+  countryService = inject(Country)
  onSearch(value:string){
     console.log({value});
+    this.countryService.searchByCapital(value).subscribe(resp => {
+      console.log({resp});
+      
+    })
     
   }
 }
