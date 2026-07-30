49e9d8ec313eb9e0a3f355b26fb32f21d21c8fde
aaron lopez <aaron_lopez222@hotmail.com>
2026-07-29 19:32:45 -0600

    feat: add Decimal Pipe to show population correctly

--- src/app/country/components/country-list/country-list.ts
+++ src/app/country/components/country-list/country-list.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { RESTCountry } from '../../interfaces/rest-countries.interfaces';
 import { Country } from '../../interfaces/country.interfaces';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-country-list',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './country-list.html',
 })
 export class CountryList {
